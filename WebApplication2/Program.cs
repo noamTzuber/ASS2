40ee4dfd18afb0e9de260ca57cc94c7e8ce30090
hilaninio <vhkv200a@gmail.com>
2022-05-24 14:20:38 +0300

    signalr add contact

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -10,22 +10,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("Allow All",
     builder =>
     {
-        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .WithMethods("GET", "POST")
+              .AllowCredentials();
     });
+   
 });
 builder.Services.AddDbContext<WebApplication2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplication2Context") ?? throw new InvalidOperationException("Connection string 'WebApplication2Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSignalR();
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
